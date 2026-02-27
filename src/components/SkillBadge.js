a325782f26acc93292cf9d6adedede0ebe64d2b4
Amani-S21 <amanitungasamuel62@gmail.com>
2026-02-27 01:12:36 -0800

    Better structure

--- src/components/SkillBadge.js
+++ src/components/SkillBadge.js
@@ -2,8 +2,8 @@ function SkillBadge({ skill }) {
   return (
     <div className="skill" style={{ backgroundColor: skill.color }}>
       {skill.skill}
-      {skill.level === "advanced" && "👌"}
-      {skill.level === "intermediate" && "😜"}
+      {skill.level === "advanced" && "💪"}
+      {skill.level === "intermediate" && "👍"}
       {skill.level === "beginner" && "😃"}
     </div>
   );
